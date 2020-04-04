22106e73b922f9f2e8b557780521da6e52d2bf07
Lucas Garron <code@garron.net>
2020-04-03 20:48:45 -0700

    Initial version.

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+window.addEventListener("load", () => {
+  const buttons = document.querySelectorAll("button");
+  const video = document.querySelector("video")!;
+  const leadIn = document.querySelector("#lead-in")! as HTMLInputElement;
+  console.log(buttons);
+  for (let i = 0; i < buttons.length; i++) {
+    const button = buttons[i];
+    button.addEventListener("click", () => {
+      video.currentTime = parseInt(button.getAttribute("data-timestamp")) - (leadIn.checked ? 6 : 0);
+      video.play();
+      video.focus();
+    })
+  }
+
+})
