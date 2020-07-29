3d5eeb74e4ed66b5b5d43757832d9243ea8eb3d5
Lucas Garron <code@garron.net>
2020-07-28 17:31:48 -0700

    Update text formatting and color.

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,17 @@
 window.addEventListener("load", () => {
-  const buttons = document.querySelectorAll("button");
+  const buttons = document.querySelectorAll("a.timestamp-link");
   const video = document.querySelector("video")!;
   const leadIn = document.querySelector("#lead-in")! as HTMLInputElement;
   console.log(buttons);
   for (let i = 0; i < buttons.length; i++) {
     const button = buttons[i];
-    button.addEventListener("click", () => {
-      video.currentTime = parseFloat(button.getAttribute("data-timestamp")) - (leadIn.checked ? 6 : 0);
+    button.addEventListener("click", (e: Event) => {
+      video.currentTime =
+        parseFloat(button.getAttribute("data-timestamp")) -
+        (leadIn.checked ? 6 : 0);
       video.play();
       video.focus();
+      e.preventDefault();
     });
   }
 });
